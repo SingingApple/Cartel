f60251bffdbef7962348949950794cf9a2345349
SingingApple <anurag.as.srivastava@gmail.com>
2021-06-11 01:11:36 +0530

    errors1

--- client/src/apis/index.js
+++ client/src/apis/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const API = axios.create({
-    baseUrl: `https://ktichen-singingapple.herokuapp.com`
+    baseURL: `https://ktichen-singingapple.herokuapp.com`
 })
 
 const config = {
